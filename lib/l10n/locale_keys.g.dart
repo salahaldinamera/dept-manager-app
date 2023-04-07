ec7d9525ae4d1b6864f42d2be51b0f34e597808c
Salah-Aldin Amera <60547056+salahaldinamera@users.noreply.github.com>
2023-04-07 05:50:41 +0300

    Created Signup Page

--- lib/l10n/locale_keys.g.dart
+++ lib/l10n/locale_keys.g.dart
@@ -2,8 +2,13 @@
 
 abstract class  LocaleKeys {
   static const APP_TITLE = 'APP_TITLE';
-  static const USERNAME = 'USERNAME';
-  static const PASSWORD = 'PASSWORD';
   static const LOGIN = 'LOGIN';
+  static const SIGNUP = 'SIGNUP';
   static const FORGOT_PASSWORD = 'FORGOT_PASSWORD';
+  static const USER_INFO_USERNAME = 'USER_INFO.USERNAME';
+  static const USER_INFO_PASSWORD = 'USER_INFO.PASSWORD';
+  static const USER_INFO_FIRST_NAME = 'USER_INFO.FIRST_NAME';
+  static const USER_INFO_LAST_NAME = 'USER_INFO.LAST_NAME';
+  static const USER_INFO_MOBILE_NUMBER = 'USER_INFO.MOBILE_NUMBER';
+  static const USER_INFO = 'USER_INFO';
 }
